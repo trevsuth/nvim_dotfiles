0ab47b0ce0d53c8e37e1f5efe4e22ff6ea087cf1
trevsuth <trevor.sutherland@gmail.com>
2025-02-10 22:26:45 -0500

    updates for working in python

--- lua/configs/lint.lua
+++ lua/configs/lint.lua
@@ -2,6 +2,7 @@ local lint = require "lint"
 
 lint.linters_by_ft = {
   lua = { "selene" },
+  python = { "flake8" },
 }
 
 lint.linters.luacheck.args = {}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,11 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require "configs.lspconfig"
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 
@@ -27,5 +32,23 @@ return {
     config = function()
       require "configs.lint"
     end,
+  },
+
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
+    end,
+    keys = {
+      { ",v", "<cmd>VenvSelect<cr>" },
+    },
   },
 }
